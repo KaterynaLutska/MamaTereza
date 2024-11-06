905f185506ba1366d44cfc41061bdfc60866a040
KaterynaLutska <ket.lutska@gmail.com>
2024-11-06 14:55:03 +0200

    code stle

--- src/contexts/StoreContext.tsx
+++ src/contexts/StoreContext.tsx
@@ -1,57 +1,54 @@
-import { FC, useEffect, useState, createContext } from "react";
-import { StoreContextProps, StoreProviderProps } from "./types/StoreContext";
-import { fetchFunds, fetchTopProject } from "@api/request";
+import { FC, createContext, useEffect, useState } from "react";
+
 import { Fund } from "@/types/Fund";
 import { ProjectTop } from "@/types/ProjectTop";
+import { fetchFunds, fetchTopProject } from "@api/request";
 
+import { StoreContextProps, StoreProviderProps } from "./types/StoreContext";
 
 const defaultState = {
   funds: [],
-	projectsTop: []
+  projectsTop: [],
 };
-
 
 export const StoreContext = createContext<StoreContextProps>(defaultState);
 
 export const StoreContextProvider: FC<StoreProviderProps> = ({ children }) => {
   const [funds, setFunds] = useState<Fund[]>([]);
-	const [projectsTop, setProjectTop] = useState<ProjectTop[]>([]);
+  const [projectsTop, setProjectTop] = useState<ProjectTop[]>([]);
 
-	 useEffect(() => {
+  useEffect(() => {
     const loadFunds = async () => {
       try {
         const data: Fund[] = await fetchFunds();
         setFunds(data);
       } catch (error) {
         console.log(error);
-      } 
+      }
     };
     loadFunds();
   }, []);
 
-	useEffect(() => {
+  useEffect(() => {
     const loadFunds = async () => {
       try {
         const data: ProjectTop[] = await fetchTopProject();
         setProjectTop(data);
       } catch (error) {
         console.log(error);
-			}
+      }
     };
     loadFunds();
   }, []);
 
-
-
-	return (
-		<StoreContext.Provider 
-		value={{
-			funds,
-			projectsTop
-		}}
-	>
-		{children}
-	</StoreContext.Provider>
-	)
-}
-
+  return (
+    <StoreContext.Provider
+      value={{
+        funds,
+        projectsTop,
+      }}
+    >
+      {children}
+    </StoreContext.Provider>
+  );
+};
